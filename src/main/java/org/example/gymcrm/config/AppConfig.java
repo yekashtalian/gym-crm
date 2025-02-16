e1709e86e2aa8e49089fab53e8653dd40b11f287
Yevhenii Kashtalian <djin.kashtalyan@gmail.com>
2025-02-16 23:53:52 +0200

    feat: add web configuration and dependencies

--- src/main/java/org/example/gymcrm/config/AppConfig.java
+++ src/main/java/org/example/gymcrm/config/AppConfig.java
@@ -3,15 +3,21 @@ package org.example.gymcrm.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.springframework.context.annotation.ComponentScan.*;
 
 @Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:application.properties")
-@ComponentScan("org.example.gymcrm")
+@ComponentScan(
+    basePackages = "org.example.gymcrm",
+    excludeFilters = {@Filter(Controller.class), @Filter(EnableWebMvc.class)})
 public class AppConfig {
   @Bean
   public ObjectMapper objectMapper() {
-    return new ObjectMapper().registerModule(new JavaTimeModule());
+    return new ObjectMapper();
   }
 }
---
+++ src/main/java/org/example/gymcrm/config/WebConfig.java
@@ -0,0 +1,10 @@
+package org.example.gymcrm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "org.example.gymcrm.web")
+public class WebConfig {}
